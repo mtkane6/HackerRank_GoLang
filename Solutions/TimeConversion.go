ba6a6ad019e5286040c641f08a53da0b77ad1341
mtkane6 <mtkane6@gmail.com>
2020-03-28 21:46:49 -0700

    Time Conversion

--- Solutions/TimeConversion.go
+++ Solutions/TimeConversion.go
@@ -8,16 +8,12 @@ import (
 func TimeConversion(s string) string {
 	if s[len(s)-2] == byte('P') || s[len(s)-2] == byte('p') {
 		if s[0] == '1' && s[1] == '2' {
-			result := s[:8]
-			return result
+			return s[:8]
 		}
 		hour, _ := (strconv.Atoi(s[:2]))
-		intHour := (hour + 12) % 24
-		result := strconv.Itoa(intHour) + s[2:]
-		return result[:8]
+		return strconv.Itoa((hour+12)%24) + s[2:8]
 	} else if s[1] == '2' {
-		result := "00" + s[2:8]
-		return result
+		return "00" + s[2:8]
 	} else {
 		return s[:8]
 	}
