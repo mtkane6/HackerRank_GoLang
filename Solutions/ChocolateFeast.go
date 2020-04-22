4c60dfbc5a44b543e1f7a74591c8b13e2b365317
mtkane6 <mtkane6@gmail.com>
2020-04-21 22:30:41 -0700

    ChocolateFeast

---
+++ Solutions/ChocolateFeast.go
@@ -0,0 +1,33 @@
+package solutions
+
+/*
+
+Little Bobby loves chocolate. He frequently goes to his favorite  store, Penny Auntie, to buy them. They are having a promotion at Penny Auntie. If Bobby saves enough wrappers, he can turn them in for a free chocolate.
+
+For example, Bobby has  to spend on bars of chocolate that cost  each. He can turn in  wrappers to receive another bar. Initially, he buys  bars and has  wrappers after eating them. He turns in  of them, leaving him with , for  more bars. After eating those two, he has  wrappers, turns in  leaving him with  wrapper and his new bar. Once he eats that one, he has  wrappers and turns them in for another bar. After eating that one, he only has  wrapper, and his feast ends. Overall, he has eaten  bars.
+
+Function Description
+
+Complete the chocolateFeast function in the editor below. It must return the number of chocolates Bobby can eat after taking full advantage of the promotion.
+
+chocolateFeast has the following parameter(s):
+
+n: an integer representing Bobby's initial amount of money
+c: an integer representing the cost of a chocolate bar
+m: an integer representing the number of wrappers he can turn in for a free bar
+
+*/
+
+// ChocolateFeast returns thre number of chocolate bars Bobby can eat
+func ChocolateFeast(n int32, c int32, m int32) int32 {
+	var bars, wrappers, remaining int32 = n / c, n / c, 0
+	for {
+		if wrappers >= m {
+			bars += wrappers / m
+			remaining = wrappers % m
+			wrappers = wrappers/m + remaining
+		} else {
+			return bars
+		}
+	}
+}
